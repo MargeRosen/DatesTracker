3eb203c09779e9fe2c4da5fb96bc1db79b31ca48
Leandra Tejedor <lmtejedor@gmail.com>
2013-07-17 11:06:12 -0700

    Generate the Rails 3 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Datestracker::Application.config.secret_token = 'f34320fe47b764b401df2eb129ddd4ca8d8728d6ca4f92b77dc3fcd484069b90e8509a48549ce7f5ddbb3088bd6961f87fe2c5400e15e5ad3e85226293c5b80d'
